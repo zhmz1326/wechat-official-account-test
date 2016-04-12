5c0020c7d9461619c4eedc3abebdbdf86a8899ab
Tei Meichi <zhmz1326@gmail.com>
2016-04-12 13:51:51 +0800

    initial commit

---
+++ src/main/java/com/huluman/woat/controller/WoatController.java
@@ -0,0 +1,50 @@
+package com.huluman.woat.controller;
+
+import com.github.sd4324530.fastweixin.message.BaseMsg;
+import com.github.sd4324530.fastweixin.message.TextMsg;
+import com.github.sd4324530.fastweixin.message.req.TextReqMsg;
+import com.github.sd4324530.fastweixin.servlet.WeixinControllerSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/weixin")
+public class WoatController extends WeixinControllerSupport {
+    private static final Logger log = LoggerFactory.getLogger(WoatController.class);
+    private static final String TOKEN = "hulumanWoatToken"; // TODO Create with Token Generator
+    private static final String APP_ID = "appId"; // TODO Create with Token Generator
+    private static final String AES_KEY = "aesKey"; // TODO Create with Token Generator
+
+    //设置TOKEN，用于绑定微信服务器
+    @Override
+    protected String getToken() {
+        return TOKEN;
+    }
+
+    //使用安全模式时设置：APPID
+    //不再强制重写，有加密需要时自行重写该方法
+    @Override
+    protected String getAppId() {
+        return APP_ID;
+    }
+
+    //使用安全模式时设置：密钥
+    //不再强制重写，有加密需要时自行重写该方法
+    @Override
+    protected String getAESKey() {
+        return AES_KEY;
+    }
+
+    //重写父类方法，处理对应的微信消息
+    @Override
+    protected BaseMsg handleTextMsg(TextReqMsg msg) {
+        String content = msg.getContent();
+        // TODO Implement it
+        // 消息类型
+        // https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421140543&token=&lang=zh_CN
+        log.debug("Message from user:{}", content);
+        return new TextMsg("Response: Thank you!");
+    }
+}
